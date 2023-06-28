1c98fafa20c23ceb9a15e2eea754c1d17fe755ab
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-28 10:27:50 +0000

    feat: add trailingSlash option to Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/tzapdoc/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tzapio', // Usually your GitHub org/user name.
